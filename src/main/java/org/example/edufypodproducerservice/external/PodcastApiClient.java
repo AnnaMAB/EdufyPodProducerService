761deeb378a3040b5f766df4b51aa662e075b1bc
AnnaB <altea200@hotmail.com>
2025-11-28 17:42:32 +0100

    lagt till loggar

--- src/main/java/org/example/edufypodproducerservice/external/PodcastApiClient.java
+++ src/main/java/org/example/edufypodproducerservice/external/PodcastApiClient.java
@@ -2,6 +2,9 @@ package org.example.edufypodproducerservice.external;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.edufypodproducerservice.converters.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
@@ -24,35 +27,50 @@ public class PodcastApiClient {
     private String podcastAssociatedApiUrl;
     @Value("${podcastExists.api.url}")
     private String podcastExistsApiUrl;
+    private final UserInfo userInfo;
+    private static final Logger F_LOG = LogManager.getLogger("functionality");
 
     @Autowired
-    public PodcastApiClient(RestClient.Builder restClientBuilder) {
+    public PodcastApiClient(RestClient.Builder restClientBuilder, UserInfo userInfo) {
         this.restClient = restClientBuilder.build();
+        this.userInfo = userInfo;
     }
 
     public boolean podcastExists(UUID podcastId) {
+        String role = userInfo.getRole();
         try {
             ResponseEntity<Boolean> podcastExistsResponse = restClient.get()
                     .uri(podcastExistsApiUrl, podcastId)
                     .retrieve()
                     .toEntity(Boolean.class);
-            if (podcastExistsResponse.getStatusCode().is2xxSuccessful()) {
+            if (podcastExistsResponse.getStatusCode().is2xxSuccessful() && podcastExistsResponse.getBody() != null) {
+                F_LOG.info("{} successfully checked if podcast exists.", role);
                 return podcastExistsResponse.getBody();
+            } else {
+                F_LOG.warn("{}: Podcast exist check failed: {}", role, podcastExistsResponse.getStatusCode());
+                throw new IllegalStateException(
+                        podcastExistsResponse.getStatusCode().toString());
             }
-            return false;
         } catch (RestClientException e) {
+            F_LOG.warn("{}: Podcast exist check failed: {}", role, e.getMessage());
             throw new IllegalStateException("Failed to check podcast " + podcastId, e);
         }
     }
 
     public boolean podcastAssociatedWithProducer(UUID podcastId, UUID producerId) {
+        String role = userInfo.getRole();
         try {
             ResponseEntity<Boolean> podAssociatedResponse = restClient.get()
                     .uri(podcastAssociatedApiUrl, podcastId, producerId)
                     .retrieve()
                     .toEntity(Boolean.class);
-            if (podAssociatedResponse.getStatusCode().is2xxSuccessful()) {
+            if (podAssociatedResponse.getStatusCode().is2xxSuccessful() && podAssociatedResponse.getBody() != null) {
+                F_LOG.info("{} successfully checked if podcast is associated with producer.", role);
                 return podAssociatedResponse.getBody();
+            } else {
+                F_LOG.warn("{}: Producer association check failed: {}", role, podAssociatedResponse.getStatusCode());
+                throw new IllegalStateException(
+                        podAssociatedResponse.getStatusCode().toString());
             }
         } catch (HttpClientErrorException e) {
             HttpStatusCode status = e.getStatusCode();
@@ -64,18 +82,21 @@ public class PodcastApiClient {
                 String message = json.path("message").asText();
                 String path = json.path("path").asText();
 
+                F_LOG.warn("{}: Failed to check producer association. error: {}", role, message);
                 throw new IllegalStateException(
                         String.format("Failed to check podcast. Status %s, %s, Path:%s",
                                 status, message, path), e);
             } catch (IOException parseEx) {
+                F_LOG.warn("{}: Failed to check producer association. error: {}", role, parseEx.getMessage());
                 throw new IllegalStateException("Failed to check podcast. Status=" + status + " body=" + body, e);
             }
         } catch (ResourceAccessException ex) {
+            F_LOG.warn("{}: Failed to check producer association. error: {}", role, ex.getMessage());
             throw new IllegalStateException("Could not connect to podcast service: " + ex.getMessage(), ex);
         } catch (RestClientException ex) {
+            F_LOG.warn("{}: Failed to check producer association. error: {}", role, ex.getMessage());
             throw new IllegalStateException("Unexpected error calling podcast service", ex);
         }
-        return false;
     }
 
 }
--- src/main/java/org/example/edufypodproducerservice/services/ProducerServiceImpl.java
+++ src/main/java/org/example/edufypodproducerservice/services/ProducerServiceImpl.java
@@ -2,6 +2,9 @@ package org.example.edufypodproducerservice.services;
 
 
 import jakarta.transaction.Transactional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.edufypodproducerservice.converters.UserInfo;
 import org.example.edufypodproducerservice.dto.ProducerDto;
 import org.example.edufypodproducerservice.entities.Producer;
 import org.example.edufypodproducerservice.external.PodcastApiClient;
@@ -23,26 +26,34 @@ public class ProducerServiceImpl implements ProducerService {
     private final ProducerRepository producerRepository;
     private final ProducerDtoConverter producerDtoConverter;
     private final PodcastApiClient podcastApiClient;
+    private final UserInfo userInfo;
+    private static final Logger F_LOG = LogManager.getLogger("functionality");
 
 
     @Autowired
-    public ProducerServiceImpl(ProducerRepository producerRepository, ProducerDtoConverter producerDtoConverter, PodcastApiClient podcastApiClient) {
+    public ProducerServiceImpl(ProducerRepository producerRepository, ProducerDtoConverter producerDtoConverter,
+                               PodcastApiClient podcastApiClient, UserInfo userInfo) {
         this.producerRepository = producerRepository;
         this.producerDtoConverter = producerDtoConverter;
         this.podcastApiClient = podcastApiClient;
+        this.userInfo = userInfo;
     }
 
     @Transactional
     @Override
     public Producer addProducer(ProducerDto producerDto) {
+        String role = userInfo.getRole();
         Producer producer = new Producer();
         if (producerDto.getName() == null || producerDto.getName().isBlank()) {
+            F_LOG.warn("{} tried to add a producer without a name.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name is required");
         }
         if(producerRepository.findByName(producerDto.getName()).isPresent()){
+            F_LOG.warn("{} tried to add a producer with a name that already exists.", role);
             throw new ResponseStatusException(HttpStatus.CONFLICT, "A producer with that name already exists");
         }
         if (producerDto.getDescription() == null || producerDto.getDescription().isBlank()) {
+            F_LOG.warn("{} tried to add a producer without a description.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Description is required");
         }
         if (producerDto.getImageUrl() != null && !producerDto.getImageUrl().isBlank()) {
@@ -56,21 +67,26 @@ public class ProducerServiceImpl implements ProducerService {
             producer.setThumbnailUrl("https://default/thumbnail.url");
         }
         if (producerDto.getPodcasts() != null && !producerDto.getPodcasts().isEmpty()) {
+            F_LOG.warn("{} tried to add podcast to a producer from the wrong endpoint.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Podcasts can't be added from this endpoint");
         }
         producer.setName(producerDto.getName());
         producer.setDescription(producerDto.getDescription());
+
+        F_LOG.info("{} added a producer with id {}.", role, producer.getId());
         return producerRepository.save(producer);
     }
 
     @Transactional
     @Override
     public Producer updateProducer(ProducerDto producerDto) {
+        String role = userInfo.getRole();
         if(producerDto.getId() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Podcast id is required");
+            F_LOG.warn("{} tried to retrieve a producer without providing an id.", role);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Producer id is required");
         }
         Producer producer = producerRepository.findById(producerDto.getId()).orElseThrow(() -> {
-            //   F_LOG.warn("{} tried to book a workout with id {} that doesn't exist.", role, workoutToBook.getId());
+            F_LOG.warn("{} tried to retrieve a producer with id {} that doesn't exist.", role, producerDto.getId());
             return new ResponseStatusException(
                     HttpStatus.NOT_FOUND,
                     String.format("No producer exists with id: %s.", producerDto.getId())
@@ -78,20 +94,21 @@ public class ProducerServiceImpl implements ProducerService {
         });
         if (producerDto.getName() != null && !producerDto.getName().equals(producer.getName())) {
             if(producerDto.getName().isBlank()) {
-                // F_LOG.warn("{} tried to update a workout with invalid title.", role);
+                F_LOG.warn("{} tried to update a producer with invalid name.", role);
                 throw new ResponseStatusException(
                         HttpStatus.BAD_REQUEST,
                         "Name can not be left blank."
                 );
             }
             if(producerRepository.findByName(producerDto.getName()).isPresent()){
+                F_LOG.warn("{} tried to change producer name to a name that already exist.", role);
                 throw new ResponseStatusException(HttpStatus.CONFLICT, "A producer with that name already exists");
             }
             producer.setName(producerDto.getName());
         }
         if (producerDto.getDescription() != null && !producerDto.getDescription().equals(producer.getDescription())) {
             if(producerDto.getDescription().isBlank()) {
-                // F_LOG.warn("{} tried to update a workout with invalid title.", role);
+                F_LOG.warn("{} tried to update a producer with invalid description.", role);
                 throw new ResponseStatusException(
                         HttpStatus.BAD_REQUEST,
                         "Description can not be left blank."
@@ -106,22 +123,27 @@ public class ProducerServiceImpl implements ProducerService {
             producer.setThumbnailUrl(producerDto.getThumbnailUrl());
         }
         if (producerDto.getPodcasts() != null && !producerDto.getPodcasts().isEmpty() && !producerDto.getPodcasts().equals(producer.getPodcasts())) {
+            F_LOG.warn("{} tried to add podcast to a producer from the wrong endpoint.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Podcasts can't be added from this endpoint");
         }
+
+        F_LOG.info("{} updated producer with id {}.", role, producer.getId());
         return producerRepository.save(producer);
     }
 
     @Transactional
     @Override
     public String deleteProducer(UUID producerId) {
+        String role = userInfo.getRole();
         if (producerId == null) {
+            F_LOG.warn("{} tried to delete a producer without providing an id.", role);
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST,
                     "Id must be provided"
             );
         }
         Producer producer = producerRepository.findById(producerId).orElseThrow(() -> {
-            //   F_LOG.warn("{} tried to book a workout with id {} that doesn't exist.", role, workoutToBook.getId());
+            F_LOG.warn("{} tried to delete a producer with id {} that doesn't exist.", role, producerId);
             return new ResponseStatusException(
                     HttpStatus.NOT_FOUND,
                     String.format("No producer exists with id: %s.", producerId)
@@ -133,32 +155,36 @@ public class ProducerServiceImpl implements ProducerService {
             for (UUID podcastId : podcasts) {
                 exists = podcastApiClient.podcastAssociatedWithProducer(podcastId, producerId);
                 if (exists) {
+                    F_LOG.warn("{} tried to delete a producer with id {} with podcasts still associated.", role, producerId);
                     throw new ResponseStatusException(
-                            HttpStatus.CONFLICT,
-                            String.format("Producer can't be deleted while podcasts are still associated.")
+                            HttpStatus.CONFLICT, String.format("Producer can't be deleted while podcasts are still associated.")
                     );
                 }
             }
         }
+
         producerRepository.deleteById(producerId);
+        F_LOG.info("{} deleted producer with id:", role, producerId);
         return String.format("Producer with Id: %s have been successfully deleted.", producerId);
     }
 
     @Override
     public ProducerDto getProducer(UUID producerId, boolean full) {
+        String role = userInfo.getRole();
         if (producerId == null) {
+            F_LOG.warn("{} tried to retrieve a producer without providing an id.", role);
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,
-                    "Id must be provided"
+                    HttpStatus.BAD_REQUEST, "Id must be provided"
             );
         }
         Producer producer = producerRepository.findById(producerId).orElseThrow(() -> {
-            //   F_LOG.warn("{} tried to book a workout with id {} that doesn't exist.", role, workoutToBook.getId());
+            F_LOG.warn("{} tried to retrieve a producer with id {} that doesn't exist.", role, producerId);
             return new ResponseStatusException(
                     HttpStatus.NOT_FOUND,
                     String.format("No producer exists with id: %s.", producerId)
             );
         });
+        F_LOG.info("{} retrieved producer with id {}.", role, producerId);
         if (full) {
             return producerDtoConverter.producerFullDtoConvert(producer);
         }else {
@@ -168,6 +194,7 @@ public class ProducerServiceImpl implements ProducerService {
 
     @Override
     public List<ProducerDto> getAllProducers(boolean full) {
+        String role = userInfo.getRole();
         List<Producer> producers = producerRepository.findAll();
         List<ProducerDto> producerDtos = new ArrayList<>();
         if (full) {
@@ -179,24 +206,27 @@ public class ProducerServiceImpl implements ProducerService {
                 producerDtos.add(producerDtoConverter.producerLimitedDtoConvert(producer));
             }
         }
+        F_LOG.info("{} retrieved all producers.", role);
         return producerDtos;
     }
 
     @Override
     public ProducerDto getProducerByPodcastId(UUID podcastId, boolean full) {
+        String role = userInfo.getRole();
         if (podcastId == null) {
+            F_LOG.warn("{} tried to retrieve a producer without providing an PodcastId.", role);
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,
-                    "PodcastId must be provided"
+                    HttpStatus.BAD_REQUEST,"PodcastId must be provided"
             );
         }
         Producer producer = producerRepository.findByPodcastsContaining(podcastId).orElseThrow(() -> {
-            //   F_LOG.warn("{} tried to book a workout with id {} that doesn't exist.", role, workoutToBook.getId());
+            F_LOG.warn("{} tried to retrieve a producer containing podcast id {} that doesn't exist.", role, podcastId);
             return new ResponseStatusException(
                     HttpStatus.NOT_FOUND,
                     String.format("No producer exists for podcast with id: %s.", podcastId)
             );
         });
+        F_LOG.info("{} retrieved producer with id {}.", role, producer.getId());
         if (full) {
             return producerDtoConverter.producerFullDtoConvert(producer);
         }else {
@@ -206,21 +236,29 @@ public class ProducerServiceImpl implements ProducerService {
 
     @Override
     public ProducerDto addPodcastToProducer(UUID producerId, UUID podcastId) {
+        String role = userInfo.getRole();
         if (producerId == null) {
+            F_LOG.warn("{} tried to add a podcast without providing producerId.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Producer ID must be provided");
         }
         if (podcastId == null) {
+            F_LOG.warn("{} tried to add a podcast without providing podcastId.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Podcast ID must be provided");
         }
         System.out.println(podcastId);
-        Producer producer = producerRepository.findById(producerId).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        String.format("No producer exists with ID: %s", producerId)));
-        if(!podcastApiClient.podcastExists(podcastId)) {
+        Producer producer = producerRepository.findById(producerId).orElseThrow(() -> {
+            F_LOG.warn("{} tried to retrieve a producer with id {} that doesn't exist.", role, producerId);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("No producer exists with ID: %s", producerId)
+                );
+            });
+            if(!podcastApiClient.podcastExists(podcastId)) {
+            F_LOG.warn("{} tried to add an podcast that does not exist.", role);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Podcast does not exist");
         }
         List<UUID> podcasts = producer.getPodcasts();
         if (podcasts.contains(podcastId)) {
+            F_LOG.warn("{} tried to add an podcast that's already has that producer", role);
             throw new ResponseStatusException(HttpStatus.CONFLICT,
                     String.format("Podcast %s already exists for producer %s", podcastId, producerId));
         }
@@ -228,31 +266,40 @@ public class ProducerServiceImpl implements ProducerService {
         producer.setPodcasts(podcasts);
         Producer saved = producerRepository.save(producer);
 
+        F_LOG.info("{} added an podcast to producer with {}.", role, producerId);
         return producerDtoConverter.producerFullDtoConvert(saved);
     }
 
     @Transactional
     @Override
     public Boolean removePodcastFromProducer(UUID producerId, UUID podcastId) {
+        String role = userInfo.getRole();
         if (producerId == null) {
+            F_LOG.warn("{} tried to remove a podcast without providing producerId.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Producer ID must be provided");
         }
         if (podcastId == null) {
+            F_LOG.warn("{} tried to add a podcast without providing podcastId.", role);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Podcast ID must be provided");
         }
-        Producer producer = producerRepository.findById(producerId).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        String.format("No producer exists with ID: %s", producerId)));
-
+        Producer producer = producerRepository.findById(producerId).orElseThrow(() -> {
+            F_LOG.warn("{} tried to retrieve a producer with id {} that doesn't exist.", role, producerId);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("No producer exists with ID: %s", producerId)
+            );
+        });
         List<UUID> podcasts = new ArrayList<>(producer.getPodcasts());
         podcasts.remove(podcastId);
         producer.setPodcasts(podcasts);
         producerRepository.save(producer);
+
+        F_LOG.info("{} removed podcast from producer with {}.", role, producerId);
         return true;
     }
 
     @Override
     public Boolean producerExist(UUID producerId) {
+        F_LOG.info("{} checked if producer exists", userInfo.getRole());
         return producerRepository.existsById(producerId);
     }
 
